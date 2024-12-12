63efd10b2a68624babcd9884b7d71b884796f5be
watari.naru <165542671+watari-naru@users.noreply.github.com>
2024-12-12 14:25:22 +0900

    Update config.lua
    Speed ​​limits can now be changed for each vehicle category

--- config.lua
+++ config.lua
@@ -1,15 +1,33 @@
 Config = {}
 
-
 -- Speed Limiter --
 Config.speedlimiter = true -- Set to false to disable the speed limiter
-Config.speedlimit = 150 -- What should the speed limit be? (MPH)
-Config.emergencyclassSpeedlimit = 200 -- What should the speed limit be for the emergency class? (MPH)
-
+Config.Compacts = 150
+Config.Sedans = 150
+Config.SUVs = 150
+Config.Coupes = 150
+Config.Muscle = 150
+Config.SportsClassics = 150
+Config.Sports = 150
+Config.Super = 150
+Config.Motorcycles = 150
+Config.OffRoad = 150
+Config.Industrial = 150
+Config.Utility = 150
+Config.Vans = 150
+Config.Cycles = 150
+Config.Boats = 150
+Config.Helicopters = 150
+Config.Planes = 150
+Config.Service = 150
+Config.Emergency = 150
+Config.Military = 150
+Config.Commercial = 150
+Config.Trains = 150
+Config.OpenWheels = 150
 
 -- Anti VDM --
 Config.antivdm = true -- Makes running people over with your vehicle do 0 damage
-
 
 -- Disable Vehicle Weapons --
 Config.disablevehicleweapons = true -- Disables all vehicle weapons
